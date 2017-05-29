9a7d7c065cc5a10d07f7b741c206b9525eeb8859
sabri1806 <sabrinita185@gmail.com>
2017-05-29 13:50:42 -0300

    agrega comentarios

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
+//inicializacion de variables
 var express = require('express'),
-    app = express(),
-    server = require('http').createServer(app),
-    io = require("socket.io").listen(server),
-    nicknames = {};
+app = express(),
+//creamos el servidor  y le pasamos app 
+server = require('http').createServer(app),
+//indicamos a io que escuche a server
+io = require("socket.io").listen(server),
+nicknames = {};
 
-//server.listen(8000);
+//server.listen(8000); * si lo corres local
+//configuramos esto para la web de hosting
 console.log(process.env.PORT);
 const port = process.env.PORT || 3000;
 const ip = process.env.IP || '127.0.0.1';
 
+
 server.listen(port, ip);
+
+//mandamos un archivo en la respuesta
 app.get('/', function(req, res) {
     res.sendfile(__dirname + '/index.html');
 });
+
+
+
 io.sockets.on('connection', function(socket) {
     socket.on('send message', function(data) {
         io.sockets.emit('new message', {msg: data, nick: socket.nickname});
@@ -29,6 +39,7 @@ io.sockets.on('connection', function(socket) {
         }
     });
     
+    //limpia user que se desconectan 
     socket.on('disconnect', function(data) {
         if(!socket.nickname) return;
         delete nicknames[socket.nickname];
